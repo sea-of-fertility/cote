80eae475c5db08c3f6f4025d8993a64fee819e5d
LetgoMoveNotLate <hj981203@gmail.com>
2024-04-22 15:40:33 +0900

    바텀업 관점으로 문제 해결

--- thiscote/chapter08/03.py
+++ thiscote/chapter08/03.py
@@ -4,11 +4,17 @@ input = sys.stdin.readline
 
 
 def solution():
-    pass
+    for i in range(n):
+        if i < 2:
+            d[i] = food[i]
+        elif 0 <= i - 3:
+            d[i] = max(d[i - 3], d[i - 2]) + food[i]
 
 
 if __name__ == "__main__":
     n = int(input())
     food = list(map(int, input().split()))
-    print(food)
+    d = [0] * 100
+    d[2] = d[1] + food[2]
     solution()
+    print(d[n-1])
