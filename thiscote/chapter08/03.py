8acd0eb3daa9355c4097bf5875eda987abe0f4eb
LetgoMoveNotLate <hj981203@gmail.com>
2024-04-23 14:07:18 +0900

    교재에 나온 코드를 이해 후 적용

--- thiscote/chapter08/03.py
+++ thiscote/chapter08/03.py
@@ -4,17 +4,15 @@ input = sys.stdin.readline
 
 
 def solution():
-    for i in range(n):
-        if i < 2:
-            d[i] = food[i]
-        elif 0 <= i - 3:
-            d[i] = max(d[i - 3], d[i - 2]) + food[i]
+    for i in range(2, n):
+        d[i] = max(d[i - 1], d[i - 2] + food[i])
 
 
 if __name__ == "__main__":
     n = int(input())
     food = list(map(int, input().split()))
     d = [0] * 100
-    d[2] = d[1] + food[2]
+    d[0] = food[0]
+    d[1] = max(food[0], food[1])
     solution()
     print(d[n-1])
