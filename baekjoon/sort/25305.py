da0ff0e69621fc390f4a0ecda592486d1b7a5da4
LetgoMoveNotLate <hj981203@gmail.com>
2024-06-02 23:59:28 +0900

    라인 줄이기

--- baekjoon/sort/25305.py
+++ baekjoon/sort/25305.py
@@ -1,7 +1,5 @@
 if __name__ == "__main__":
     n, m = map(int, input().split())
-
-    arr = list(map(int, input().split()))
-    arr.sort(reverse=True)
-
+    arr = list(map(int, input().split())).sort(reverse=True)
     print(arr[m-1])
+
