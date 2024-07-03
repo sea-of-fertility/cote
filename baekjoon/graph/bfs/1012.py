a53edb6aa94087e90af4508b8b6df1814f5eec17
LetgoMoveNotLate <hj981203@gmail.com>
2024-07-03 16:13:52 +0900

    bfs

---
+++ baekjoon/graph/bfs/1012.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+dx_dy = [[1, 0], [0, 1], [-1, 0],[0, -1]]
+
+
+def bfs(col, row):
+    de = deque([(col, row)])  # Using tuple here
+    while de:
+        dy, dx = de.popleft()
+        graph[dy][dx] = False
+        for d in dx_dy:
+            dy += d[0]
+            dx += d[1]
+            if 0 <= dy < n and 0 <= dx < m and graph[dy][dx]:
+                graph[dy][dx] = False
+                de.append((dy, dx))
+
+
+def solution():
+    answer = 0
+    for col in range(n):
+        for row in range(m):
+            if graph[col][row]:
+                bfs(col, row)
+                answer += 1
+    return answer
+
+
+if __name__ == "__main__":
+    case = int(input())
+    for _ in range(case):
+        m, n, k = map(int, input().split())
+        graph = [[0 for _ in range(m)] for _ in range(n)]
+
+        for _ in range(k):
+            x, y = map(int, input().split())
+            graph[y][x] = True
+        print(solution())
