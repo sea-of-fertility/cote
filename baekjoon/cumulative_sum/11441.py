8d979b0f7aee9610aeea51506bf45e724ec46406
LetgoMoveNotLate <hj981203@gmail.com>
2024-07-06 14:07:51 +0900

    cumulative_sum을 전역변수로 사용

--- baekjoon/cumulative_sum/11441.py
+++ baekjoon/cumulative_sum/11441.py
@@ -4,11 +4,10 @@ import sys
 input = sys.stdin.readline
 
 
-def cumulative():
-    cumulative_sum = [0 for _ in range(n+1)]
+def cumulative(cur_sum):
     for index, item in enumerate(arr):
-        cumulative_sum[index+1] = cumulative_sum[index] + item
-    return cumulative_sum
+        cur_sum[index+1] = cur_sum[index] + item
+    return cur_sum
 
 
 def solution(i, j):
@@ -19,7 +18,8 @@ if __name__ == "__main__":
     n = int(input())
     arr = list(map(int, input().split()))
     m = int(input())
-    cumulative_sum = cumulative()
+    cumulative_sum = [0 for _ in range(n+1)]
+    cumulative(cumulative_sum)
     for _ in range(m):
         start, end = map(int, input().split())
         solution(start, end)
