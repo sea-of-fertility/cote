b2ace58731be220dd206fd832119418004f0c8b6
LetgoMoveNotLate <hj981203@gmail.com>
2024-07-13 00:26:15 +0900

    우선 순위 큐(1715): 두 번째 해결 방법

--- baekjoon/priorityque/1715.py
+++ baekjoon/priorityque/1715.py
@@ -6,22 +6,17 @@ input = sys.stdin.readline
 
 def solution():
     heapq.heapify(arr)
-    answer = 0
-    poket = heapq.heappop(arr)
-    while len(arr) > 0:
-        if poket != 0:
-            heappop = heapq.heappop(arr)
-            process = poket + heappop
-            heapq.heappush(arr, process)
-            answer += process
-            poket = 0
-        else:
-            poket = heapq.heappop(arr)
-    return  answer
+    result = 0
+
+    while len(arr) > 1:
+        one = heapq.heappop(arr)
+        two = heapq.heappop(arr)
+        result += one + two
+        heapq.heappush(arr, one + two)
+    print(result)
 
 
 if __name__ == "__main__":
     n = int(input())
     arr = [int(input().strip()) for _ in range(n)]
-
-    print(solution())
+    solution()
