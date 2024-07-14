120116332f63bfb92591c69abcbdf86be4cd992f
LetgoMoveNotLate <hj981203@gmail.com>
2024-07-14 16:26:00 +0900

    grid: 어려웠다. 다시 풀어봐도 좋은 문제이다.

--- baekjoon/grid/1931.py
+++ baekjoon/grid/1931.py
@@ -3,27 +3,22 @@ import sys
 input = sys.stdin.readline
 
 
-def check(arr):
-    for time in arr:
-
-
 def solution(arr):
     result = 0
+    end = 0
     for lecture in arr:
-        if check(lecture):
-
-
+        if end <= lecture[0]:
+            end = lecture[1]
+            result += 1
+    print(result)
 
 
 def init():
     cnt = int(input())
     lecture_time = [list(map(int, input().split())) for _ in range(cnt)]
-    lecture_time.sort(key=lambda x: (x[1] - x[0], x[0]))
+    lecture_time.sort(key=lambda x: (x[1], x[0]))
     solution(lecture_time)
 
 
 if __name__ == "__main__":
-
     init()
-
-
