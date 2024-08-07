8b96df2c696bb7dc7aca442bf99888cd486e3414
LetgoMoveNotLate <hj981203@gmail.com>
2024-08-08 01:55:48 +0900

    dp: 매우 dp적인 사고능력을 물어보는 문제이다.

---
+++ baekjoon/dp/1912.py
@@ -0,0 +1,24 @@
+import sys
+
+
+input = sys.stdin.readline
+
+
+def solution(n, arr):
+    result = min(arr)
+    dp = [0] * n
+
+    dp[0] = arr[0]
+    for index in range(1, n):
+        dp[index] = max(dp[index-1] + arr[index], arr[index])
+    print(result)
+
+
+def main():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    solution(n, arr)
+
+
+if __name__ == "__main__":
+    main()
